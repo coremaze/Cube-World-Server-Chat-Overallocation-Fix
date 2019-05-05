295dd24d91c5bf0ab7290055add82984e9837029
ChrisMiuchiz <chris000005@gmail.com>
2019-05-04 21:49:03 -0400

    Treat length as signed and add warning message

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <windows.h>
-
+#include <iostream>
 unsigned int base;
 unsigned int invalid_JMP_back;
 unsigned int valid_JMP_back;
 void __declspec(naked) __declspec(noinline) MaliciousChatChecker(){
     asm("cmp eax, 512"); //limit chat length to 512 bytes
-    asm("jg 0f"); //end communication with client
+    asm("ja 0f"); //end communication with client
 
     asm("push 0"); //original code
     asm("push eax");
@@ -14,6 +14,8 @@ void __declspec(naked) __declspec(noinline) MaliciousChatChecker(){
     asm("jmp [_valid_JMP_back]"); //continue as normal
 
     asm("0:");
+    asm("push eax"); //length
+    asm("call [_warn_ptr]");
     asm("jmp [_invalid_JMP_back]"); //impossible to deref in jg
 
 }
@@ -24,6 +26,12 @@ void WriteJMP(BYTE* location, BYTE* newFunction){
     *((DWORD*)(location + 1)) = (DWORD)(( (unsigned INT32)newFunction - (unsigned INT32)location ) - 5);
     VirtualProtect(location, 5, dwOldProtection, &dwOldProtection);
 }
+
+void __stdcall warn(unsigned int length){
+    printf("[Warning] A player tried to send a chat packet using a length of 0x%X, but the attempt was blocked.\n", length);
+    return;
+}
+void* warn_ptr = (void*)&warn;
 
 extern "C" __declspec(dllexport) bool APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
